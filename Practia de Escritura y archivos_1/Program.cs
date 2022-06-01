8a29f7ad836fd75f8fb270f05321bd1bce3443af
AngelMancilla <angelmancilla0114@gmail.com>
2022-06-01 11:12:59 -0700

    Practica Escritura y archivos_1.0

---
+++ Practia de Escritura y archivos_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practia_de_Escritura_y_archivos_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creacion del archivo
+            StreamWriter sw = new StreamWriter("Ejemplo.txt", true);
+
+            string[] lines =
+            {
+                "Este es un nuevo texto",
+                "Hola",
+                "Bonito dia :)"
+            };
+
+            //Recorrer archivo para escribir las lineas en el archivo
+            foreach(string line in lines)
+            {
+                //escribir en el archivo
+                sw.WriteLine(line);
+            }
+
+            //Cerrar archivo
+            sw.Close();
+
+            Console.WriteLine("Escribiendo...");
+            Console.ReadLine();
+        }
+    }
+}
